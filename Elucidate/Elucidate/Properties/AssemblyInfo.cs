58d6f86de7be2e6da74689dc795bde3c2107bbdd
BlueBlock <danb@aspitel.com>
2018-08-02 07:35:35 -0400

    fix wix installer to use x64 build

--- Elucidate/Elucidate/Properties/AssemblyInfo.cs
+++ Elucidate/Elucidate/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 //      Build Number  - Increment
 //      Revision      - Day
 //
-[assembly: AssemblyVersion("18.8.2.528")]
-[assembly: AssemblyFileVersion("18.8.2.528")]
+[assembly: AssemblyVersion("18.8.2.530")]
+[assembly: AssemblyFileVersion("18.8.2.530")]
