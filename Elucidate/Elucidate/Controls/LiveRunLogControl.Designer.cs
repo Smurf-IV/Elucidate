bc9b55efb1ef7050db1dfc441228814208df1b73
BlueBlock <danb@aspitel.com>
2018-08-03 19:41:36 -0400

    initial code for LiveLog refactoring

---
+++ Elucidate/Elucidate/Controls/LiveRunLogControl.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Elucidate.Controls
+{
+    partial class LiveRunLogControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelCommandLineOptions = new System.Windows.Forms.Label();
+            this.txtAddCommands = new System.Windows.Forms.TextBox();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.runWithoutCaptureMenuItem = new System.Windows.Forms.CheckBox();
+            this.toolStripProgressBar1 = new Elucidate.Shared.TextOverProgressBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.toolStripProgressBar1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.toolStripStatusLabel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.runWithoutCaptureMenuItem, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 385);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(871, 27);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.txtAddCommands, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelCommandLineOptions, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 358);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(871, 27);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // labelCommandLineOptions
+            // 
+            this.labelCommandLineOptions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelCommandLineOptions.Location = new System.Drawing.Point(3, 0);
+            this.labelCommandLineOptions.MinimumSize = new System.Drawing.Size(175, 20);
+            this.labelCommandLineOptions.Name = "labelCommandLineOptions";
+            this.labelCommandLineOptions.Size = new System.Drawing.Size(175, 27);
+            this.labelCommandLineOptions.TabIndex = 5;
+            this.labelCommandLineOptions.Text = "&Additional Command line options:";
+            this.labelCommandLineOptions.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtAddCommands
+            // 
+            this.txtAddCommands.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtAddCommands.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAddCommands.Location = new System.Drawing.Point(182, 7);
+            this.txtAddCommands.Margin = new System.Windows.Forms.Padding(1);
+            this.txtAddCommands.MaxLength = 128;
+            this.txtAddCommands.Name = "txtAddCommands";
+            this.txtAddCommands.Size = new System.Drawing.Size(100, 13);
+            this.txtAddCommands.TabIndex = 6;
+            this.txtAddCommands.WordWrap = false;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = true;
+            this.toolStripStatusLabel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripStatusLabel1.Margin = new System.Windows.Forms.Padding(0);
+            this.toolStripStatusLabel1.MinimumSize = new System.Drawing.Size(132, 28);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(136, 28);
+            this.toolStripStatusLabel1.TabIndex = 1;
+            this.toolStripStatusLabel1.Text = "2011-06-012T18:54:54";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Stopped",
+            "Running",
+            "Abort",
+            "Idle"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(113, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // runWithoutCaptureMenuItem
+            // 
+            this.runWithoutCaptureMenuItem.AutoSize = true;
+            this.runWithoutCaptureMenuItem.Location = new System.Drawing.Point(258, 3);
+            this.runWithoutCaptureMenuItem.Name = "runWithoutCaptureMenuItem";
+            this.runWithoutCaptureMenuItem.Size = new System.Drawing.Size(80, 17);
+            this.runWithoutCaptureMenuItem.TabIndex = 4;
+            this.runWithoutCaptureMenuItem.Text = "checkBox1";
+            this.runWithoutCaptureMenuItem.UseVisualStyleBackColor = true;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.ContainerControl = this;
+            this.toolStripProgressBar1.DisplayText = "";
+            this.toolStripProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripProgressBar1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(205)))), ((int)(((byte)(50)))));
+            this.toolStripProgressBar1.Location = new System.Drawing.Point(341, 0);
+            this.toolStripProgressBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.ShowInTaskbar = true;
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(530, 27);
+            this.toolStripProgressBar1.Step = 3;
+            this.toolStripProgressBar1.TabIndex = 5;
+            this.toolStripProgressBar1.TextColor = System.Drawing.SystemColors.ControlText;
+            // 
+            // LiveRunLogControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LiveRunLogControl";
+            this.Size = new System.Drawing.Size(871, 412);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label labelCommandLineOptions;
+        private System.Windows.Forms.TextBox txtAddCommands;
+        private System.Windows.Forms.Label toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Shared.TextOverProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.CheckBox runWithoutCaptureMenuItem;
+    }
+}
---
+++ Elucidate/Elucidate/Controls/LiveRunLogControl.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Elucidate.Logging;
+using Elucidate.wyDay.Controls;
+
+namespace Elucidate.Controls
+{
+    public partial class LiveRunLogControl : UserControl
+    {
+        public LiveRunLogControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// eventing idea taken from http://stackoverflow.com/questions/1423484/using-bashcygwin-inside-c-program
+        /// </summary>
+        private readonly ManualResetEvent mreProcessExit = new ManualResetEvent(false);
+        private readonly ManualResetEvent mreOutputDone = new ManualResetEvent(false);
+        private readonly ManualResetEvent mreErrorDone = new ManualResetEvent(false);
+        private ProcessPriorityClass requested = ProcessPriorityClass.Normal;
+        private string _lastError;
+
+        private readonly BackgroundWorker actionWorker = new BackgroundWorker
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true,
+        };
+
+        private class ThreadObject
+        {
+            public BackgroundWorker BgdWorker;
+            public Process CmdProcess;
+        }
+
+        internal void StartSnapRaidProcess(string command)
+        {
+            Log.Instance.Debug("Command buttcon clicked but a command is still running.");
+            if (actionWorker.IsBusy) return;
+            //UseWaitCursor = true;
+            //EnableIfValid(false);
+            comboBox1.Enabled = true;
+            comboBox1.Text = @"Running";
+            requested = ProcessPriorityClass.Normal;
+            actionWorker.RunWorkerAsync(command);
+            toolStripStatusLabel1.Text = DateTime.Now.ToString("u");
+            toolStripProgressBar1.DisplayText = $"{command} - Starting";
+            toolStripProgressBar1.State = ProgressBarState.Normal;
+            toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+            toolStripProgressBar1.Value = 0;
+        }
+
+
+        private void actionWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                Log.Instance.Debug("actionWorker_DoWork");
+                BackgroundWorker worker = sender as BackgroundWorker;
+                string command = e.Argument as string;
+
+                if (worker == null)
+                {
+                    Log.Instance.Error("Passed in worker is null");
+                    e.Cancel = true;
+                    return;
+                }
+                else if (string.IsNullOrWhiteSpace(command))
+                {
+                    Log.Instance.Error("Passed in command is null");
+                    e.Cancel = true;
+                    return;
+                }
+
+                int exitCode;
+                mreProcessExit.Reset();
+                mreOutputDone.Reset();
+                mreErrorDone.Reset();
+                string args = Util.FormatSnapRaidCommandArgs(command, txtAddCommands.Text, out string appPath);
+
+                if (runWithoutCaptureMenuItem.Checked)
+                {
+                    Log.Instance.Warn("Running without Capture mode");
+                    Log.Instance.Info("Starting minimised {0} {1}", appPath, args);
+                    Log.Instance.Info("Working...");
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "CMD.exe",
+                        Arguments = $" /K \"{appPath} {args}\"",
+                        WorkingDirectory = $"{Path.GetDirectoryName(Properties.Settings.Default.SnapRAIDFileLocation)}",
+                        UseShellExecute = true,
+                        WindowStyle = ProcessWindowStyle.Minimized,
+                        ErrorDialog = true
+                    };
+                    Process process = Process.Start(startInfo);
+                    if (process != null) Log.Instance.Info("Process is running PID[{0}]", process.Id);
+                    return;
+                }
+
+                using (Process process = new Process
+                {
+                    StartInfo = new ProcessStartInfo(appPath, args)
+                    {
+                        UseShellExecute = false,      // We're redirecting !
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        StandardOutputEncoding = Encoding.UTF8,   // SnapRAID uses UTF-8 for output
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = true
+                    },
+                    EnableRaisingEvents = true
+                })
+                {
+                    Log.Instance.Info("Using: {0}", process.StartInfo.FileName);
+                    Log.Instance.Info("with: {0}", process.StartInfo.Arguments);
+                    process.Exited += Exited;
+
+                    process.Start();
+                    // Redirect the output stream of the child process.
+                    ThreadObject threadObject = new ThreadObject { BgdWorker = worker, CmdProcess = process };
+                    ThreadPool.QueueUserWorkItem(o => ReadStandardOutput(threadObject));
+                    ThreadPool.QueueUserWorkItem(o => ReadStandardError(threadObject));
+
+                    if (process.HasExited)
+                    {
+                        mreProcessExit.Set();
+                    }
+
+                    while (!WaitHandle.WaitAll(new WaitHandle[] { mreErrorDone, mreOutputDone, mreProcessExit }, 250))
+                    {
+                        if (process.HasExited) continue;
+                        if (worker.CancellationPending)
+                        {
+                            Log.Instance.Fatal("Attempting process KILL");
+                            process.Kill();
+                        }
+                        else
+                        {
+                            ProcessPriorityClass current = process.PriorityClass;
+                            if (current == requested) continue;
+                            Log.Instance.Fatal("Setting the processpriority to[{0}]", requested);
+                            process.PriorityClass = requested;
+                        }
+                    }
+
+                    exitCode = process.ExitCode;
+                    if (exitCode == 0)
+                    {
+                        Log.Instance.Info("ExitCode=[{0}]", exitCode);
+                    }
+                    else
+                    {
+                        Log.Instance.Error("ExitCode=[{0}]", exitCode);
+                    }
+                    process.Close();
+                }
+                switch (exitCode)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        worker.ReportProgress(101, "Error");
+                        break;
+                    default:
+                        Log.Instance.Debug($"Unhandled ExitCode of {exitCode}");
+                        break;
+                }
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.ReportException(ex, "Thread closing abnormally");
+                throw;
+            }
+        }
+
+
+        private void ReadStandardError(ThreadObject threadObject)
+        {
+            try
+            {
+                Log.Instance.Info("Start Verbose handler");
+                string buf;
+                do
+                {
+                    if (!string.IsNullOrEmpty(buf = threadObject.CmdProcess.StandardError.ReadLine()))
+                    {
+                        _lastError = buf;
+                        Log.Instance.Warn("Verbose[{0}]", buf);
+                    }
+                    else
+                    {
+                        mreProcessExit.WaitOne(100);
+                    }
+                } while (!string.IsNullOrEmpty(buf)
+                         || !mreProcessExit.WaitOne(0) // If millisecondsTimeout is zero, the method does not block. It tests the state of the wait handle and returns immediately.
+                );
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.ReportException(ex, "Thread closing abnormally");
+            }
+            mreErrorDone.Set();
+        }
+
+        private void ReadStandardOutput(ThreadObject threadObject)
+        {
+            try
+            {
+                Log.Instance.Debug("Start StdOut handler");
+                string buf;
+                do
+                {
+                    if (!string.IsNullOrEmpty(buf = threadObject.CmdProcess.StandardOutput.ReadLine()))
+                    {
+                        Log.Instance.Info("StdOut[{0}]", buf);
+                        if (!buf.Contains("%")) continue;
+                        string[] splits = buf.Split('%');
+                        if (int.TryParse(splits[0], out int percentProgress))
+                        {
+                            threadObject.BgdWorker.ReportProgress(percentProgress, buf);
+                        }
+                    }
+                    else
+                    {
+                        mreProcessExit.WaitOne(100);
+                    }
+                    //Log.Instance.Debug("sleep");
+                    //Thread.Sleep(1000);
+                } while (!string.IsNullOrEmpty(buf)
+                         || !mreProcessExit.WaitOne(0) // If millisecondsTimeout is zero, the method does not block. It tests the state of the wait handle and returns immediately.
+                );
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.ReportException(ex, "ReadStandardOutput: ");
+            }
+            mreOutputDone.Set();
+        }
+
+        private void Exited(object o, EventArgs e)
+        {
+            Log.Instance.Debug("Exited.");
+            Thread.Sleep(1000);  // When the process has exited, the buffers are _still_ flushing
+            mreProcessExit.Set();
+        }
+
+        private void actionWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (toolStripProgressBar1.Style == ProgressBarStyle.Marquee)
+            {
+                toolStripProgressBar1.Style = ProgressBarStyle.Continuous;
+            }
+            if (e.ProgressPercentage < 101)
+            {
+                toolStripProgressBar1.DisplayText = e.UserState as string;
+                toolStripProgressBar1.Value = e.ProgressPercentage;
+            }
+            else if (e.ProgressPercentage == 101)
+            {
+                toolStripProgressBar1.State = ProgressBarState.Error;
+                toolStripProgressBar1.DisplayText = _lastError;
+                toolStripProgressBar1.Value = 100;
+            }
+        }
+
+        private void actionWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //UseWaitCursor = false;
+            //EnableIfValid(true);
+
+            if (toolStripProgressBar1.Style == ProgressBarStyle.Marquee)
+            {
+                toolStripProgressBar1.Style = ProgressBarStyle.Continuous;
+            }
+            if (e.Cancelled)
+            {
+                toolStripProgressBar1.State = ProgressBarState.Pause;
+                toolStripProgressBar1.DisplayText = "Cancelled";
+                Log.Instance.Error("The thread has been cancelled");
+                comboBox1.Text = @"Abort";
+            }
+            else if (e.Error != null)
+            {
+                toolStripProgressBar1.State = ProgressBarState.Error;
+                toolStripProgressBar1.DisplayText = e.Error.Message;
+                Log.Instance.Error(e.Error, "Thread threw: ");
+                comboBox1.Text = @"Abort";
+            }
+            else
+            {
+                //if (toolStripProgressBar1.State == ProgressBarState.Normal)
+                {
+                    toolStripProgressBar1.DisplayText = "Completed";
+                    toolStripProgressBar1.Value = 100;
+                    comboBox1.Text = @"Stopped";
+                }
+                //Log.Instance.Info("Completed");
+            }
+            comboBox1.Enabled = false;
+        }
+
+
+
+
+
+
+
+    }
+}
