02386315478751a9e4190a9d1419947ad1612cbe
BlueBlock <danb@aspitel.com>
2018-08-09 15:17:47 -0400

    change installer to not default to starting the app on the user logon
    remove the shortcut from default installing into startup. we'll look at adding this function as an option in the menu of the app

--- Elucidate/Elucidate/Properties/AssemblyInfo.cs
+++ Elucidate/Elucidate/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 //      Build Number  - Increment
 //      Revision      - Day
 //
-[assembly: AssemblyVersion("18.8.9.114")]
-[assembly: AssemblyFileVersion("18.8.9.114")]
+[assembly: AssemblyVersion("18.8.9.118")]
+[assembly: AssemblyFileVersion("18.8.9.118")]
