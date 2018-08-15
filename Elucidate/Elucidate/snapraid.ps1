53e211f9923b039b080941357d5f4d73244d7edd
BlueBlock <danb@aspitel.com>
2018-08-15 14:21:21 -0400

    add powershell script to help executing snapraid scheduled tasks
    the script will check only one snapraid runs so we do not have overalpping and conflicting scheduled tasks operating on the raid. There is a corresponding check in the app

---
+++ Elucidate/Elucidate/snapraid.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$exe,
+    [Parameter(Mandatory=$true)][string]$args
+ )
+
+$args = $args -replace "<DATETIME>", (Get-Date).tostring("yyyyMMdd_HHmmss")
+
+$loopStart = get-date
+
+do {
+    if((get-process "snapraid" -ea SilentlyContinue) -eq $Null) {
+		Write-Host "To have this window hidden:
+		Write-Host "Edit the Sheduled Task and in Security Options "
+		Write-Host "select 'Run whether the user is logged on or not'"
+		Write-Host "and also make sure the 'Hiden' checkbox is checked."
+		Write-Host ""
+		Write-Host "Running SnapRAID process..."
+        Start-Process -FilePath $exe -ArgumentList $args -Wait -WindowStyle Hidden
+		Write-Host "Done"
+        Exit
+    } else {
+        Write-Host "Another SnapRAID process is already running."
+        Start-Sleep 10
+    }
+}
+while((Get-Date) -le $loopStart.AddMinutes(5))
+
+Write-Host "This run attempt failed to run for 5 minutes. Another SnapRAID process was already running."
