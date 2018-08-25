56a877b25770cd339811ee0501fdc9ac84084b99
BlueBlock <danb@aspitel.com>
2018-08-24 20:55:37 -0400

    update solution

--- Elucidate/Elucidate/Properties/AssemblyInfo.cs
+++ Elucidate/Elucidate/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 //      Build Number  - Increment
 //      Revision      - Day
 //
-[assembly: AssemblyVersion("18.8.25.14")]
-[assembly: AssemblyFileVersion("18.8.25.14")]
+[assembly: AssemblyVersion("18.8.25.15")]
+[assembly: AssemblyFileVersion("18.8.25.15")]
