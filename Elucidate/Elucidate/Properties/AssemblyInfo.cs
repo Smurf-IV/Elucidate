c450c1d76761595063daef62a8cd453e0c7204dd
Smurf-IV <smurf.iv@gmail.com>
2018-09-29 16:12:42 +0100

    - Fix Merge

--- Elucidate/Elucidate/Properties/AssemblyInfo.cs
+++ Elucidate/Elucidate/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 //      Build Number  - Increment
 //      Revision      - Day
 //
-[assembly: AssemblyVersion("18.8.27.3")]
-[assembly: AssemblyFileVersion("18.8.27.3")]
+[assembly: AssemblyVersion("2018.9.604.29")]
+[assembly: AssemblyFileVersion("18.9.604.29")]
